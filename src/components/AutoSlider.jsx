ace6e2774bf2ccd9ba6235707e4704afac60a276
Tejas Katkar <tkatkar1256@gmail.com>
2025-12-25 01:21:00 +0530

    update the slider some major task

--- src/components/AutoSlider.jsx
+++ src/components/AutoSlider.jsx
@@ -20,25 +20,27 @@ export default function SmoothInfiniteSlider() {
   const looped = [...images, ...images, ...images, ...images];
 
   return (
-    <div className="relative w-2/3 ml-0 pl-0 overflow-hidden">
-      
-      {/* Left blur */}
-      <div className="absolute left-0 top-0 h-full w-20 bg-linear-to-r from-white via-white/70 to-transparent z-20 pointer-events-none"></div>
+  <div className="relative w-full lg:w-2/3 ml-0 pl-0 overflow-hidden">
 
-      {/* Right blur */}
-      <div className="absolute right-0 top-0 h-full w-20 bg-linear-to-l from-white via-white/70 to-transparent z-20 pointer-events-none"></div>
+    {/* Left blur (DESKTOP ONLY) */}
+    <div className="hidden lg:block absolute left-0 top-0 h-full w-20 bg-linear-to-r from-white via-white/70 to-transparent z-20 pointer-events-none"></div>
 
-      {/* Infinite track */}
-      <div className="smooth-track flex gap-4 py-4">
-        {looped.map((src, i) => (
-          <img
-            key={i}
-            src={src}
-            draggable="false"
-            className="w-[180px] h-[120px] rounded-xl object-cover shadow-md shrink-0"
-          />
-        ))}
-      </div>
+    {/* Right blur (DESKTOP ONLY) */}
+    <div className="hidden lg:block absolute right-0 top-0 h-full w-20 bg-linear-to-l from-white via-white/70 to-transparent z-20 pointer-events-none"></div>
+
+    {/* Infinite track */}
+    <div className="smooth-track flex gap-2 lg:gap-4 py-2 lg:py-4">
+      {looped.map((src, i) => (
+        <img
+          key={i}
+          src={src}
+          draggable="false"
+          className="w-[120px] h-20 lg:w-[180px] lg:h-[120px] rounded-lg lg:rounded-xl object-cover shadow-sm lg:shadow-md shrink-0"
+        />
+      ))}
     </div>
-  );
+
+  </div>
+);
+
 }
